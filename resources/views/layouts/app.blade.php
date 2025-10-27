b831b4799be05b0f7ce6f2fdcf215feed0ecb8a4
Jon Nila <algajon123@gmail.com>
2025-10-27 03:16:06 +0100

    Render deploy: Laravel + FastAPI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,14 @@
     </style>
     @yield('styles')
 </head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FJ8K2FQJRK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FJ8K2FQJRK');
+</script>
 <body class="bg-white">
     @include('include.header')
     
